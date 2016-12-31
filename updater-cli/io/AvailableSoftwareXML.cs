4fb574d4d292346a338653219f803064535dae06
Dirk Stolle <striezel-dev@web.de>
2016-12-31 20:06:09 +0100

    implement XML write method for AvailableSoftware class

--- updater-cli/io/AvailableSoftwareXML.cs
+++ updater-cli/io/AvailableSoftwareXML.cs
@@ -27,7 +27,7 @@ namespace updater_cli.io
     /// <summary>
     /// reads a list of available software from an XML file
     /// </summary>
-    public class AvailableSoftwareReaderXML
+    public class AvailableSoftwareXML
     {
         /// <summary>
         /// tries to read AvailableSoftware elements from an XML file
@@ -36,7 +36,7 @@ namespace updater_cli.io
         /// <param name="data">collection that will be used to save the elements that were read from the XML file</param>
         /// <returns>Returns true, if the read operation was successful.
         /// Returns false, if the read operation failed.</returns>
-        public static bool readFromFile(string filename, ref List<data.AvailableSoftware> data)
+        public static bool read(string filename, ref List<data.AvailableSoftware> data)
         {
             //Null, empty or whitespace strings are not a valid file name.
             if (string.IsNullOrWhiteSpace(filename))
@@ -109,6 +109,69 @@ namespace updater_cli.io
             }
             reader.Close();
             reader = null;
+            return success;
+        }
+
+
+        /// <summary>
+        /// tries to write a list of AvailableSoftware instances to an XML file
+        /// </summary>
+        /// <param name="filename">output path of the XML file</param>
+        /// <param name="data">collection that shall be written to the XML file</param>
+        /// <returns>Returns true, if the write operation was successful.
+        /// Returns false, if the write operation failed.</returns>
+        public static bool write(string filename, List<data.AvailableSoftware> data)
+        {
+            //Null, empty or whitespace strings are not a valid file name.
+            if (string.IsNullOrWhiteSpace(filename))
+                return false;
+            //List must not be null, because we need data from it.
+            if (null == data)
+                return false;
+
+            XmlWriter writer = null;
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Encoding = Encoding.UTF8;
+            settings.Indent = true;
+            try
+            {
+                writer = XmlWriter.Create(filename, settings);
+            }
+            catch (Exception)
+            {
+                //Something bad happened here. Time to exit.
+                return false;
+            }
+
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(data.AvailableSoftware));
+            try
+            {
+                writer.WriteStartElement("softwarelist");
+            }
+            catch (Exception)
+            {
+                writer.Close();
+                writer = null;
+                return false;
+            }
+            //write data elements
+            foreach (var item in data)
+            {
+                serializer.Serialize(writer, item);
+            } //foreach
+            //read the end element
+            bool success = false;
+            try
+            {
+                writer.WriteEndElement();
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+            writer.Close();
+            writer = null;
             return success;
         }
     } //class
