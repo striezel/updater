7f3f9f95f5ddca9081de5dbcac751100f3b26c75
Dirk Stolle <striezel-dev@web.de>
2017-02-12 01:15:27 +0100

    implement searchForNewer() method for KeePass

--- updater-cli/software/KeePass.cs
+++ updater-cli/software/KeePass.cs
@@ -18,6 +18,8 @@
 
 using updater_cli.data;
 using System;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace updater_cli.software
 {
@@ -52,7 +54,7 @@ namespace updater_cli.software
         /// exception in the later case.</returns>
         public bool implementsSearchForNewer()
         {
-            return false;
+            return true;
         }
 
 
@@ -63,7 +65,61 @@ namespace updater_cli.software
         /// that was retrieved from the net.</returns>
         public AvailableSoftware searchForNewer()
         {
-            throw new NotImplementedException();
+            string htmlCode = null;
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    htmlCode = client.DownloadString("http://keepass.info/integrity.html");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception occurred while checking for newer version of KeePass: " + ex.Message);
+                    return null;
+                }
+                client.Dispose();
+            } //using
+
+            Regex reExe = new Regex("&lt;KeePass\\-[2-9]\\.[0-9]{2}\\-Setup\\.exe&gt;");
+            Match matchExe = reExe.Match(htmlCode);
+            if (!matchExe.Success)
+                return null;
+            //MSI follows after .exe
+            Regex reMsi = new Regex("&lt;KeePass\\-[2-9]\\.[0-9]{2}\\.msi&gt;");
+            Match matchMsi = reMsi.Match(htmlCode, matchExe.Index + 1);
+            if (!matchMsi.Success)
+                return null;
+            //extract new version number
+            string newVersion = matchExe.Value.Replace("&lt;KeePass-", "").Replace("-Setup.exe&gt;", "");
+            //version number should match usual scheme, e.g. 2.xx, where xx are two digits
+            Regex version = new Regex("^[2-9]\\.[0-9]{2}$");
+            if (!version.IsMatch(newVersion))
+                return null;
+
+            //extract hash
+            Regex hash = new Regex("SHA256       \\: [0-9A-F ]+");
+            Match matchHash = hash.Match(htmlCode, matchExe.Index + 1);
+            if (!matchHash.Success)
+                return null;
+            if (matchHash.Index > matchMsi.Index)
+                return null;
+            //find second part of hash
+            Regex hash2 = new Regex("[0-9A-F ]+");
+            Match matchHash2 = hash2.Match(htmlCode, matchHash.Index + matchHash.Length);
+            if (!matchHash2.Success)
+                return null;
+            if (matchHash2.Index > matchMsi.Index)
+                return null;
+            string newHash = matchHash.Value.Replace("SHA256       : ", "").Trim()
+                + " " + matchHash2.Value.Trim();
+            //construct new version information
+            var newInfo = info();
+            //replace version number - both as newest version and in URL for download
+            string oldVersion = newInfo.newestVersion;
+            newInfo.newestVersion = newVersion;
+            newInfo.install32Bit.downloadUrl = newInfo.install32Bit.downloadUrl.Replace(oldVersion, newVersion);
+            newInfo.install32Bit.checksum = newHash;
+            return newInfo;
         }
 
     } //class
