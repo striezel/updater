acd81b9640dbcf09dc421bd1cffe302945ce5d6d
Dirk Stolle <striezel-dev@web.de>
2017-02-18 20:53:31 +0100

    set assembly version

--- updater-cli/Properties/AssemblyInfo.cs
+++ updater-cli/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("2017.02.18.0")]
+[assembly: AssemblyFileVersion("2017.02.18.0")]
