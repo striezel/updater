6765cbfe18a259df878b25be2b8b3ab7087f7ab4
Dirk Stolle <striezel-dev@web.de>
2017-03-15 17:30:24 +0100

    add utility class to determine executable format

---
+++ updater-cli/utility/PortableExecutable.cs
@@ -0,0 +1,130 @@
+﻿/*
+    This file is part of the updater command line interface.
+    Copyright (C) 2017  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.IO;
+
+namespace updater_cli.utility
+{
+    public enum PEFormat
+    {
+        /// <summary>
+        /// unknown format
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// not a portable executable file
+        /// </summary>
+        NotPE,
+
+        /// <summary>
+        /// 32 bit executable
+        /// </summary>
+        PE32,
+
+        /// <summary>
+        /// 64 bit executable
+        /// </summary>
+        PE64
+    }
+
+
+    /// <summary>
+    /// class to determine format of portable executables
+    /// </summary>
+    public class PortableExecutable
+    {
+        /// <summary>
+        /// NLog.Logger for PortableExecutable class
+        /// </summary>
+        private static NLog.Logger logger = NLog.LogManager.GetLogger(typeof(PortableExecutable).FullName);
+
+
+        /// <summary>
+        /// determines the executable format of the given file
+        /// </summary>
+        /// <param name="fileName">path and name of the executable file</param>
+        /// <returns>Returns enumeration value to indicate the executable format.</returns>
+        public static PEFormat determineFormat(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                return PEFormat.NotPE;
+            if (!File.Exists(fileName))
+                return PEFormat.NotPE;
+
+            ushort machineType = 0;
+
+            try
+            {
+                //Inspired by http://stackoverflow.com/a/1002672
+
+                // Offset to PE header is located at 0x3C.
+                // The PE header starts with "PE\0\0" =  0x50 0x45 0x00 0x00,
+                // followed by a 2-byte machine type field.
+                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                try
+                {
+                    BinaryReader br = new BinaryReader(fs);
+                    try
+                    {
+                        fs.Seek(0x3c, SeekOrigin.Begin);
+                        Int32 peOffset = br.ReadInt32();
+                        fs.Seek(peOffset, SeekOrigin.Begin);
+                        UInt32 peHead = br.ReadUInt32();
+
+                        if (peHead != 0x00004550) // "PE\0\0", little-endian
+                            return PEFormat.NotPE;
+                        //read machine type field
+                        machineType = br.ReadUInt16();
+                    } //try-fin
+                    finally
+                    {
+                        br.Close();
+                        br = null;
+                    } //finally
+                } //try-fin
+                finally
+                {
+                    fs.Close();
+                    fs = null;
+                } //finally
+            } //try-catch
+            catch (Exception ex)
+            {
+                logger.Error("Error ("+ ex.GetType().Name + ") while determining executable type of \""
+                    + fileName + "\": " + ex.Message);
+                return PEFormat.NotPE;
+            }
+
+            switch (machineType)
+            {
+                case 0:
+                    return PEFormat.Unknown;
+                case 0x8664: //AMD 64
+                case 0x200: //Itanium 64
+                    return PEFormat.PE64;
+                case 0x14c: //i386
+                    return PEFormat.PE32;
+                default:
+                    return PEFormat.Unknown;
+            } //switch
+        }
+
+    } //class
+} //namespace
---
+++ updater-test/utility/PortableExecutable_Tests.cs
@@ -0,0 +1,82 @@
+﻿/*
+    This file is part of the updater command line interface.
+    Copyright (C) 2017  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using updater_cli.utility;
+
+namespace updater_test.utility
+{
+    [TestClass]
+    public class PortableExecutable_Tests
+    {
+        /// <summary>
+        /// checks whether PortableExecutable.determineFormat() can properly detect
+        /// the format of a 32 bit portable executable
+        /// </summary>
+        [TestMethod]
+        public void Test_determineFormat_cmd()
+        {
+            //cmd.exe should always be 32 bit.
+            var format = PortableExecutable.determineFormat("C:\\Windows\\System32\\cmd.exe");
+            Assert.AreEqual(PEFormat.PE32, format);
+        }
+
+
+        /// <summary>
+        /// checks whether PortableExecutable.determineFormat() can properly detect
+        /// the format of a 64 bit portable executable
+        /// </summary>
+        [TestMethod]
+        public void Test_determineFormat_executable64()
+        {
+            if (!Environment.Is64BitOperatingSystem)
+            {
+                Assert.Inconclusive("Probably there will not be an 64 bit executables on a 32 bit operating system,"
+                    + " so we cannot test a 64 bit executable.");
+                return;
+            }
+
+            //explorer.exe should be 64 bit on a 64 bit OS.
+            var format = PortableExecutable.determineFormat("C:\\Windows\\explorer.exe");
+            Assert.AreEqual(PEFormat.PE64, format);
+        }
+
+
+        /// <summary>
+        /// checks whether PortableExecutable.determineFormat() can handle null,
+        /// empty and whitespace values gracefully
+        /// </summary>
+        [TestMethod]
+        public void Test_determineFormat_NullEmptyWhitespace()
+        {
+            //null
+            var format = PortableExecutable.determineFormat(null);
+            Assert.AreNotEqual(PEFormat.PE32, format);
+            Assert.AreNotEqual(PEFormat.PE64, format);
+            //empty
+            format = PortableExecutable.determineFormat("");
+            Assert.AreNotEqual(PEFormat.PE32, format);
+            Assert.AreNotEqual(PEFormat.PE64, format);
+            //whitespace
+            format = PortableExecutable.determineFormat("   \t   \r\n  \r  \n   \v  ");
+            Assert.AreNotEqual(PEFormat.PE32, format);
+            Assert.AreNotEqual(PEFormat.PE64, format);
+        }
+    } //class
+} //namespace
