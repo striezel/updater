b3d8f82efd6df2021597eb715a1b3bcfd8793a15
Dirk Stolle <striezel-dev@web.de>
2017-03-18 17:11:25 +0100

    preparations for Shockwave Player updates

---
+++ updater-cli/software/Shockwave.cs
@@ -0,0 +1,91 @@
+﻿/*
+    This file is part of the updater command line interface.
+    Copyright (C) 2017  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using updater_cli.data;
+
+namespace updater_cli.software
+{
+    public class Shockwave : NoPreUpdateProcessSoftware
+    {
+        /// <summary>
+        /// NLog.Logger for Shockwave class
+        /// </summary>
+        private static NLog.Logger logger = NLog.LogManager.GetLogger(typeof(Shockwave).FullName);
+
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        /// <param name="autoGetNewer">whether to automatically get
+        /// newer information about the software when calling the info() method</param>
+        public Shockwave(bool autoGetNewer)
+            : base(autoGetNewer)
+        { }
+
+
+        /// <summary>
+        /// gets the currently known information about the software
+        /// </summary>
+        /// <returns>Returns an AvailableSoftware instance with the known
+        /// details about the software.</returns>
+        public override AvailableSoftware knownInfo()
+        {
+            return new AvailableSoftware("Adobe Shockwave Player", "12.2.8.198",
+                "^Adobe Shockwave Player [0-9]+\\.[0-9]+$",
+                null,
+                //Shockwave Player only has one installer.
+                new InstallInfoMsi(
+                    "http://fpdownload.macromedia.com/get/shockwave/default/english/win95nt/latest/sw_lic_full_installer.msi",
+                    HashAlgorithm.SHA512,
+                    "51f548e54203be5f24373dd5f65a8b5bdb1a877e15959e4303a9545d498c0235e852e2e9efa4b3be4ad73034c3284ae42728c4e13f5b85d97f1689145d33401a",
+                    "/qn /norestart",
+                    null,
+                    "C:\\Windows\\syswow64\\Adobe\\Director"),
+                //There is no 64 bit installer.
+                null);
+        }
+
+
+        /// <summary>
+        /// whether or not the method searchForNewer() is implemented
+        /// </summary>
+        /// <returns>Returns true, if searchForNewer() is implemented for that
+        /// class. Returns false, if not. Calling searchForNewer() may throw an
+        /// exception in the later case.</returns>
+        public override bool implementsSearchForNewer()
+        {
+            return false;
+        }
+
+
+        /// <summary>
+        /// looks for newer versions of the software than the currently known version
+        /// </summary>
+        /// <returns>Returns an AvailableSoftware instance with the information
+        /// that was retrieved from the net.</returns>
+        public override AvailableSoftware searchForNewer()
+        {
+            logger.Debug("Searching for newer version of Shockwave Player...");
+            return null;
+        }
+
+    } //class
+} //namespace
---
+++ updater-test/software/Shockwave_Tests.cs
@@ -0,0 +1,70 @@
+﻿/*
+    This file is part of the updater command line interface.
+    Copyright (C) 2017  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using updater_cli.software;
+
+namespace updater_test.software
+{
+    /// <summary>
+    /// unit tests for software.Shockwave class
+    /// </summary>
+    [TestClass]
+    public class Shockwave_Tests : BasicSoftwareTests
+    {
+        /// <summary>
+        /// checks whether info() returns some meaningful data
+        /// </summary>
+        [TestMethod]
+        public void Test_info()
+        {
+            _info(new Shockwave(false));
+        }
+
+
+        /// <summary>
+        /// checks whether the class implements the searchForNewer() method
+        /// </summary>
+        [TestMethod]
+        public void Test_implementsSearchForNewer()
+        {
+            var shock = new Shockwave(false);
+            Assert.IsTrue(shock.implementsSearchForNewer());
+        }
+
+
+        /// <summary>
+        /// checks whether searchForNewer() returns something
+        /// </summary>
+        [TestMethod]
+        public void Test_searchForNewer()
+        {
+            _searchForNewer(new Shockwave(false));
+        }
+
+
+        /// <summary>
+        /// checks whether the class info is up to date
+        /// </summary>
+        [TestMethod]
+        public void Test_upToDate_info()
+        {
+            _upToDate_info(new Shockwave(false));
+        }
+    } //class
+} //namespace
