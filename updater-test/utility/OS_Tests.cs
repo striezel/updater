95b81b5f779ac083a6aad9fab625abc57624f933
Dirk Stolle <striezel-dev@web.de>
2017-07-23 16:37:26 +0200

    add utility class to determine OS

---
+++ updater-test/utility/OS_Tests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using updater.utility;
+
+namespace updater_test.utility
+{
+    /// <summary>
+    /// contains unit tests for updater.utility.OS class
+    /// </summary>
+    [TestClass]
+    public class OS_Tests
+    {
+        private static OperatingSystem win2000()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(5, 0));
+        }
+
+        private static OperatingSystem winXP()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(5, 1));
+        }
+
+        private static OperatingSystem winXP64()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(5, 2));
+        }
+
+        private static OperatingSystem winVista()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(6, 0));
+        }
+
+        private static OperatingSystem win7()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(6, 1));
+        }
+
+        private static OperatingSystem win8()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(6, 2));
+        }
+
+        private static OperatingSystem win8_1()
+        {
+            return new OperatingSystem(PlatformID.Win32NT, new Version(6, 3));
+        }
+
+
+        [TestMethod]
+        public void Test_isWinXPOrNewer()
+        {
+            Assert.IsFalse(OS.isWinXPOrNewer(win2000()));
+            Assert.IsTrue(OS.isWinXPOrNewer(winXP()));
+            Assert.IsTrue(OS.isWinXPOrNewer(winXP64()));
+            Assert.IsTrue(OS.isWinXPOrNewer(winVista()));
+            Assert.IsTrue(OS.isWinXPOrNewer(win7()));
+            Assert.IsTrue(OS.isWinXPOrNewer(win8()));
+            Assert.IsTrue(OS.isWinXPOrNewer(win8_1()));
+        }
+
+
+        [TestMethod]
+        public void Test_isWinVistaOrNewer()
+        {
+            Assert.IsFalse(OS.isWinVistaOrNewer(win2000()));
+            Assert.IsFalse(OS.isWinVistaOrNewer(winXP()));
+            Assert.IsFalse(OS.isWinVistaOrNewer(winXP64()));
+            Assert.IsTrue(OS.isWinVistaOrNewer(winVista()));
+            Assert.IsTrue(OS.isWinVistaOrNewer(win7()));
+            Assert.IsTrue(OS.isWinVistaOrNewer(win8()));
+            Assert.IsTrue(OS.isWinVistaOrNewer(win8_1()));
+        }
+
+
+        [TestMethod]
+        public void Test_isWin7OrNewer()
+        {
+            Assert.IsFalse(OS.isWin7OrNewer(win2000()));
+            Assert.IsFalse(OS.isWin7OrNewer(winXP()));
+            Assert.IsFalse(OS.isWin7OrNewer(winXP64()));
+            Assert.IsFalse(OS.isWin7OrNewer(winVista()));
+            Assert.IsTrue(OS.isWin7OrNewer(win7()));
+            Assert.IsTrue(OS.isWin7OrNewer(win8()));
+            Assert.IsTrue(OS.isWin7OrNewer(win8_1()));
+        }
+    } //class
+} //namespace
---
+++ updater/utility/OS.cs
@@ -0,0 +1,86 @@
+﻿/*
+    This file is part of the updater command line interface.
+    Copyright (C) 2017  Dirk Stolle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+
+namespace updater.utility
+{
+    /// <summary>
+    /// utility class to get operating system information
+    /// </summary>
+    public class OS
+    {
+        /// <summary>
+        /// information about the current operating system
+        /// </summary>
+        private static readonly OperatingSystem thisOS = Environment.OSVersion;
+
+
+        /* See https://msdn.microsoft.com/en-us/library/windows/desktop/ms724832(v=vs.85).aspx
+         * for information on different version numbers. */
+
+
+        /// <summary>
+        /// checks whether the system is Windows XP or newer
+        /// </summary>
+        /// <param name="os">operating system information
+        /// (Leave this at null, if you want to examine the current OS.)</param>
+        /// <returns>Returns true, if the OS is WinXP or a newer version.</returns>
+        public static bool isWinXPOrNewer(OperatingSystem os = null)
+        {
+            if (null == os)
+                os = thisOS;
+            //5.1 (32bit) or 5.2 (64bit) is WinXP
+            return (os.Platform == PlatformID.Win32NT)
+                && ((os.Version.Major > 5)
+                || ((os.Version.Major == 5) && (os.Version.Minor >= 1)));
+        }
+
+
+        /// <summary>
+        /// checks whether the system is Windows Vista or newer
+        /// </summary>
+        /// <param name="os">operating system information
+        /// (Leave this at null, if you want to examine the current OS.)</param>
+        /// <returns>Returns true, if the OS is Vista or a newer version.</returns>
+        public static bool isWinVistaOrNewer(OperatingSystem os = null)
+        {
+            if (null == os)
+                os = thisOS;
+            //6.0
+            return (os.Platform == PlatformID.Win32NT) && (os.Version.Major >= 6);
+        }
+
+
+        /// <summary>
+        /// checks whether the system is Windows 7 or newer
+        /// </summary>
+        /// <param name="os">operating system information
+        /// (Leave this at null, if you want to examine the current OS.)</param>
+        /// <returns>Returns true, if the OS is Win7 or a newer version.</returns>
+        public static bool isWin7OrNewer(OperatingSystem os = null)
+        {
+            if (null == os)
+                os = thisOS;
+            //6.1
+            return (os.Platform == PlatformID.Win32NT) 
+                && ((os.Version.Major > 6)
+                || ((os.Version.Major == 6) && (os.Version.Minor >=1)));
+        }
+    } //class
+} //namespace
