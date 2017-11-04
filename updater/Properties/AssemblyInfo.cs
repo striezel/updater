b8b37bde64c88b3239d3db7cab2ea5eca50c27ed
Dirk Stolle <striezel-dev@web.de>
2017-11-05 00:37:39 +0100

    bump version to 2017.11.05.0

--- updater/Properties/AssemblyInfo.cs
+++ updater/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2017.10.10.0")]
-[assembly: AssemblyFileVersion("2017.10.10.0")]
+[assembly: AssemblyVersion("2017.11.05.0")]
+[assembly: AssemblyFileVersion("2017.11.05.0")]
