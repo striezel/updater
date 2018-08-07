ce4a0c88c32864161737dc0a4337d6dc147163dd
Dirk Stolle <striezel-dev@web.de>
2018-08-07 17:13:51 +0200

    update known information for WinSCP to version 5.13.3

--- updater/software/WinSCP.cs
+++ updater/software/WinSCP.cs
@@ -33,7 +33,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// default constructor
+        /// Constructor.
         /// </summary>
         /// <param name="autoGetNewer">whether to automatically get
         /// newer information about the software when calling the info() method</param>
@@ -43,19 +43,19 @@ namespace updater.software
 
 
         /// <summary>
-        /// gets the currently known information about the software
+        /// Gets the currently known information about the software.
         /// </summary>
         /// <returns>Returns an AvailableSoftware instance with the known
         /// details about the software.</returns>
         public override AvailableSoftware knownInfo()
         {
             return new AvailableSoftware("WinSCP",
-                "5.13.1",
+                "5.13.3",
                 "^WinSCP [1-9]+\\.[0-9]+\\.[0-9]+$", null,
                 new InstallInfoExe(
-                    "https://netix.dl.sourceforge.net/project/winscp/WinSCP/5.13.1/WinSCP-5.13.1-Setup.exe",
+                    "https://netix.dl.sourceforge.net/project/winscp/WinSCP/5.13.3/WinSCP-5.13.3-Setup.exe",
                     HashAlgorithm.SHA256,
-                    "34911e4c59f29e3174190bae9a57a5698c4e825e77daad2d8e03c45de7537858",
+                    "58eeeb8454b8a7d194e3b58a1d6f2fb501906b8f1c3f2716a4a6d15e16164b86",
                     "CN=Martin Prikryl, O=Martin Prikryl, L=Prague, S=Hlavni mesto Praha, C=CZ",
                     "/VERYSILENT /NORESTART"),
                 //There is no 64 bit installer yet.
@@ -74,7 +74,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// whether or not the method searchForNewer() is implemented
+        /// Determines whether or not the method searchForNewer() is implemented.
         /// </summary>
         /// <returns>Returns true, if searchForNewer() is implemented for that
         /// class. Returns false, if not. Calling searchForNewer() may throw an
@@ -86,7 +86,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// looks for newer versions of the software than the currently known version
+        /// Looks for newer versions of the software than the currently known version.
         /// </summary>
         /// <returns>Returns an AvailableSoftware instance with the information
         /// that was retrieved from the net.</returns>
@@ -112,18 +112,18 @@ namespace updater.software
             Match matchExe = reExe.Match(htmlCode);
             if (!matchExe.Success)
                 return null;
-            //extract new version number
+            // extract new version number
             string newVersion = matchExe.Value.Replace("WinSCP-", "").Replace("-Setup.exe", "");
             versions.Triple newTriple = new versions.Triple(newVersion);
             versions.Triple oldTriple = new versions.Triple(knownInfo().newestVersion);
             if (newTriple < oldTriple)
                 return null;
-            //version number should match usual scheme, e.g. 5.x.y, where x and y are digits
+            // version number should match usual scheme, e.g. 5.x.y, where x and y are digits
             Regex version = new Regex("^[1-9]+\\.[0-9]+\\.[0-9]+$");
             if (!version.IsMatch(newVersion))
                 return null;
 
-            //Readme (e.g. https://winscp.net/download/WinSCP-5.9.5-ReadMe.txt) contains hash.
+            // Readme (e.g. https://winscp.net/download/WinSCP-5.9.5-ReadMe.txt) contains hash.
             htmlCode = null;
             using (var client = new WebClient())
             {
@@ -137,15 +137,15 @@ namespace updater.software
                     return null;
                 }
                 client.Dispose();
-            } //using
-            //extract hash - .exe occurs first, so first hash is the one we want
+            } // using
+            // extract hash - .exe occurs first, so first hash is the one we want
             Regex hash = new Regex("SHA\\-256\\: [0-9a-f]{64}");
             Match matchHash = hash.Match(htmlCode);
             if (!matchHash.Success)
                 return null;
             string newHash = matchHash.Value.Replace("SHA-256: ", "").Trim();
 
-            //The "file" https://winscp.net/download/WinSCP-5.9.5-Setup.exe or
+            // The "file" https://winscp.net/download/WinSCP-5.9.5-Setup.exe or
             // similar is just a HTML page that starts the download of the real
             // file after a few seconds, so we have to parse the direct link of
             // the download and use that.
@@ -162,8 +162,9 @@ namespace updater.software
                     return null;
                 }
                 client.Dispose();
-            } //using
-            //URL for direct download is something like
+            } // using
+
+            // URL for direct download is something like
             // https://winscp.net/download/files/201704212143f42467fc64e4c84259bce4a07a98edbd/WinSCP-5.9.5-Setup.exe,
             // where the middle part (date plus random MD5 hash?) varies.
             Regex downloadUrl = new Regex(Regex.Escape("https://winscp.net/download/files/")
@@ -173,9 +174,9 @@ namespace updater.software
             if (!matchUrl.Success)
                 return null;
 
-            //construct new version information
+            // construct new version information
             var newInfo = knownInfo();
-            //replace version number - both as newest version and in URL for download
+            // replace version number - both as newest version and in URL for download
             string oldVersion = newInfo.newestVersion;
             newInfo.newestVersion = newVersion;
             newInfo.install32Bit.downloadUrl = matchUrl.Value;
@@ -185,8 +186,8 @@ namespace updater.software
 
 
         /// <summary>
-        /// lists names of processes that might block an update, e.g. because
-        /// the application cannot be update while it is running
+        /// Lists names of processes that might block an update, e.g. because
+        /// the application cannot be update while it is running.
         /// </summary>
         /// <param name="detected">currently installed / detected software version</param>
         /// <returns>Returns a list of process names that block the upgrade.</returns>
@@ -197,5 +198,5 @@ namespace updater.software
             return li;
         }
 
-    } //class
-} //namespace
+    } // class
+} // namespace
