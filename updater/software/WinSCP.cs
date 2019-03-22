673affead3389b5d59ecdf3a3d2549acf25b3da6
Dirk Stolle <striezel-dev@web.de>
2019-03-22 23:08:32 +0100

    update known information for WinSCP to version 5.13.9

--- updater/software/WinSCP.cs
+++ updater/software/WinSCP.cs
@@ -50,12 +50,12 @@ namespace updater.software
         public override AvailableSoftware knownInfo()
         {
             return new AvailableSoftware("WinSCP",
-                "5.13.8",
+                "5.13.9",
                 "^WinSCP [1-9]+\\.[0-9]+\\.[0-9]+$", null,
                 new InstallInfoExe(
-                    "https://netix.dl.sourceforge.net/project/winscp/WinSCP/5.13.8/WinSCP-5.13.8-Setup.exe",
+                    "https://netix.dl.sourceforge.net/project/winscp/WinSCP/5.13.9/WinSCP-5.13.9-Setup.exe",
                     HashAlgorithm.SHA256,
-                    "63b53f5faa76376a110301af4431ecb786c0083d0ba4bf34fc3d0f1c0b8b8c49",
+                    "948a67cf4dbb11f1c5af302511b7942c579f6a51b1b09e39f92725e4b71d2689",
                     "CN=Martin Prikryl, O=Martin Prikryl, L=Prague, S=Hlavni mesto Praha, C=CZ",
                     "/VERYSILENT /NORESTART"),
                 //There is no 64 bit installer yet.
