a77b8c1c8283a759640a3dd487d9a0052ae2e18b
Dirk Stolle <striezel-dev@web.de>
2019-07-12 01:44:12 +0200

    some code formatting

--- updater/software/Pidgin.cs
+++ updater/software/Pidgin.cs
@@ -33,7 +33,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// default constructor
+        /// Default constructor.
         /// </summary>
         /// <param name="autoGetNewer">whether to automatically get
         /// newer information about the software when calling the info() method</param>
@@ -43,7 +43,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// gets the currently known information about the software
+        /// Gets the currently known information about the software.
         /// </summary>
         /// <returns>Returns an AvailableSoftware instance with the known
         /// details about the software.</returns>
@@ -53,7 +53,7 @@ namespace updater.software
                 "2.12.0",
                 "^Pidgin$",
                 null,
-                //Pidgin only has an installer for 32 bit.
+                // Pidgin only has an installer for 32 bit.
                 new InstallInfoPidgin(
                     "https://netcologne.dl.sourceforge.net/project/pidgin/Pidgin/2.12.0/pidgin-2.12.0-offline.exe",
                     HashAlgorithm.SHA256,
@@ -66,7 +66,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// list of IDs to identify the software
+        /// Gets a collection of IDs to identify the software.
         /// </summary>
         /// <returns>Returns a non-empty array of IDs, where at least one entry is unique to the software.</returns>
         public override string[] id()
@@ -76,7 +76,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// whether or not the method searchForNewer() is implemented
+        /// Determines whether or not the method searchForNewer() is implemented.
         /// </summary>
         /// <returns>Returns true, if searchForNewer() is implemented for that
         /// class. Returns false, if not. Calling searchForNewer() may throw an
@@ -88,7 +88,7 @@ namespace updater.software
 
 
         /// <summary>
-        /// looks for newer versions of the software than the currently known version
+        /// Looks for newer versions of the software than the currently known version.
         /// </summary>
         /// <returns>Returns an AvailableSoftware instance with the information
         /// that was retrieved from the net.</returns>
@@ -116,13 +116,13 @@ namespace updater.software
                 return null;
             string version = matchVersion.Value.Replace("<span class=\"number\">", "").Replace("</span>", "");
             
-            //No checksum, only signature.
+            // No checksum, only signature.
 
-            //construct new information
+            // construct new information
             var newInfo = knownInfo();
             string oldVersion = newInfo.newestVersion;
             newInfo.newestVersion = version;
-            //32 bit
+            // 32 bit
             newInfo.install32Bit.downloadUrl = newInfo.install32Bit.downloadUrl.Replace(oldVersion, version);
             newInfo.install32Bit.checksum = null;
             newInfo.install32Bit.algorithm = HashAlgorithm.Unknown;
@@ -131,8 +131,8 @@ namespace updater.software
 
 
         /// <summary>
-        /// lists names of processes that might block an update, e.g. because
-        /// the application cannot be update while it is running
+        /// Lists names of processes that might block an update, e.g. because
+        /// the application cannot be update while it is running.
         /// </summary>
         /// <param name="detected">currently installed / detected software version</param>
         /// <returns>Returns a list of process names that block the upgrade.</returns>
@@ -141,5 +141,5 @@ namespace updater.software
             return new List<string>();
         }
 
-    } //class
-} //namespace
+    } // class
+} // namespace
