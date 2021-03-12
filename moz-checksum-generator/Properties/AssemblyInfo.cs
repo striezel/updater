4e8868ea4b0e02f42389cae06ddbba093a7f75cf
Dirk Stolle <striezel-dev@web.de>
2021-03-12 12:02:47 +0100

    moz-checksum-generator: update assembly description

--- moz-checksum-generator/Properties/AssemblyInfo.cs
+++ moz-checksum-generator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("moz-checksum-generator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility to generates checksums for Mozilla Firefox / SeaMonkey / Thunderbird setups.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("moz-checksum-generator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2021 Dirk Stolle")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.0")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
