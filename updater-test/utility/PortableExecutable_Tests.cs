9b53d14b606ed664194f99adf9ce0c01168d04d8
Dirk Stolle <striezel-dev@web.de>
2022-08-11 14:41:53 +0200

    fix 32 bit test case for PortableExecutable.determineFormat()

--- updater-test/utility/PortableExecutable_Tests.cs
+++ updater-test/utility/PortableExecutable_Tests.cs
@@ -1,6 +1,6 @@
 ﻿/*
     This file is part of the updater command line interface.
-    Copyright (C) 2017  Dirk Stolle
+    Copyright (C) 2017, 2022  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,30 +17,49 @@
 */
 
 using System;
+using System.IO;
+using System.Net.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using updater.utility;
 
 namespace updater_test.utility
 {
+    /// <summary>
+    /// Contains tests for the PortableExecutable utility class.
+    /// </summary>
     [TestClass]
     public class PortableExecutable_Tests
     {
         /// <summary>
-        /// checks whether PortableExecutable.determineFormat() can properly detect
-        /// the format of a 32 bit portable executable
+        /// Checks whether PortableExecutable.determineFormat() can properly detect
+        /// the format of a 32 bit portable executable.
         /// </summary>
         [TestMethod]
-        public void Test_determineFormat_cmd()
+        public void Test_determineFormat_executable32()
         {
-            //cmd.exe should always be 32 bit.
-            var format = PortableExecutable.determineFormat("C:\\Windows\\System32\\cmd.exe");
+            // Download https://the.earth.li/~sgtatham/putty/0.77/w32/putty.exe, that is a known 32 bit file.
+            string local_path = Path.Combine(Path.GetTempPath(), "known-32bit-exe.bak");
+            using (var client = new HttpClient())
+            {
+                using (var stream = client.GetStreamAsync("https://the.earth.li/~sgtatham/putty/0.77/w32/putty.exe"))
+                {
+                    using (var file_stream = new FileStream(local_path, FileMode.OpenOrCreate))
+                    {
+                        stream.Result.CopyTo(file_stream);
+                    }
+                }
+            }
+
+            // Downloaded .exe should always be 32 bit.
+            var format = PortableExecutable.determineFormat(local_path);
+            File.Delete(local_path);
             Assert.AreEqual(PEFormat.PE32, format);
         }
 
 
         /// <summary>
-        /// checks whether PortableExecutable.determineFormat() can properly detect
-        /// the format of a 64 bit portable executable
+        /// Checks whether PortableExecutable.determineFormat() can properly detect
+        /// the format of a 64 bit portable executable.
         /// </summary>
         [TestMethod]
         public void Test_determineFormat_executable64()
@@ -52,7 +71,7 @@ namespace updater_test.utility
                 return;
             }
 
-            //explorer.exe should be 64 bit on a 64 bit OS.
+            // explorer.exe should be 64 bit on a 64 bit OS.
             var format = PortableExecutable.determineFormat("C:\\Windows\\explorer.exe");
             Assert.AreEqual(PEFormat.PE64, format);
         }
@@ -65,18 +84,18 @@ namespace updater_test.utility
         [TestMethod]
         public void Test_determineFormat_NullEmptyWhitespace()
         {
-            //null
+            // null
             var format = PortableExecutable.determineFormat(null);
             Assert.AreNotEqual(PEFormat.PE32, format);
             Assert.AreNotEqual(PEFormat.PE64, format);
-            //empty
+            // empty
             format = PortableExecutable.determineFormat("");
             Assert.AreNotEqual(PEFormat.PE32, format);
             Assert.AreNotEqual(PEFormat.PE64, format);
-            //whitespace
+            // whitespace
             format = PortableExecutable.determineFormat("   \t   \r\n  \r  \n   \v  ");
             Assert.AreNotEqual(PEFormat.PE32, format);
             Assert.AreNotEqual(PEFormat.PE64, format);
         }
-    } //class
-} //namespace
+    } // class
+} // namespace
