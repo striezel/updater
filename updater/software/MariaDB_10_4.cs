9441a930f609feea4bd123325b29d79a063ca531
Dirk Stolle <striezel-dev@web.de>
2023-06-07 23:28:24 +0200

    update known information for MariaDB 10.4 to version 10.4.30

--- updater/software/MariaDB_10_4.cs
+++ updater/software/MariaDB_10_4.cs
@@ -43,7 +43,7 @@ namespace updater.software
         /// details about the software.</returns>
         public override AvailableSoftware knownInfo()
         {
-            const string version = "10.4.29";
+            const string version = "10.4.30";
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("MariaDB Server 10.4",
                 version,
@@ -53,7 +53,7 @@ namespace updater.software
                 new InstallInfoMsi(
                     "https://downloads.mariadb.org/rest-api/mariadb/" + version + "/mariadb-" + version + "-winx64.msi",
                     HashAlgorithm.SHA256,
-                    "5d775743614894375a5a311667413979dd8be0eb19b6ad1075c10ebf1d928d3e",
+                    "0f123cc0019619617d109fbbc9d4d06aa56819081f303feac806438ec6ca6cff",
                     signature,
                     "/qn /norestart")
                 );
