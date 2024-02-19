c16b62c88d621998d6a5c307e555839e26ec2b29
Dirk Stolle <striezel-dev@web.de>
2024-02-19 21:48:18 +0100

    update known information for MariaDB 10.5 to version 10.5.24

--- updater/software/MariaDB_10_5.cs
+++ updater/software/MariaDB_10_5.cs
@@ -1,6 +1,6 @@
 ﻿/*
     This file is part of the updater command line interface.
-    Copyright (C) 2022, 2023  Dirk Stolle
+    Copyright (C) 2022, 2023, 2024  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -27,6 +27,18 @@ namespace updater.software
     public sealed class MariaDB_10_5: MariaDB_Base
     {
         /// <summary>
+        /// publisher of signed binaries for MariaDB 10.5
+        /// </summary>
+        private new const string publisherX509 = "CN=\"MariaDB USA, Inc.\", O=\"MariaDB USA, Inc.\", L=Redwood City, S=California, C=US";
+
+
+        /// <summary>
+        /// expiration date of the certificate
+        /// </summary>
+        private static new readonly DateTime certificateExpiration = new(2026, 3, 21, 23, 59, 59, DateTimeKind.Utc);
+
+
+        /// <summary>
         /// Default constructor.
         /// </summary>
         /// <param name="autoGetNewer">whether to automatically get newer
@@ -43,7 +55,7 @@ namespace updater.software
         /// details about the software.</returns>
         public override AvailableSoftware knownInfo()
         {
-            const string version = "10.5.23";
+            const string version = "10.5.24";
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("MariaDB Server 10.5",
                 version,
@@ -53,7 +65,7 @@ namespace updater.software
                 new InstallInfoMsi(
                     "https://downloads.mariadb.org/rest-api/mariadb/" + version + "/mariadb-" + version + "-winx64.msi",
                     HashAlgorithm.SHA256,
-                    "9df1bf425352ee4361adffa901c3b6eb823372b3cb1299728700174cee711cd8",
+                    "835780777ba5357dd04679599da169cf489a2e7df6730b2834945938a11af92e",
                     signature,
                     "/qn /norestart")
                 );
