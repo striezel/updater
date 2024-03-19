d2d3ec1a49310f417646ceb7edafe9c7550daa95
Dirk Stolle <striezel-dev@web.de>
2024-03-19 01:56:52 +0100

    update known information for Opera GX to version 107.0.5045.75

--- updater/software/OperaGX.cs
+++ updater/software/OperaGX.cs
@@ -66,19 +66,19 @@ namespace updater.software
             const string silentOptions = "/silent /norestart /launchopera 0 /setdefaultbrowser 0 /enable-stats 0 /enable-installer-stats 0 /pintotaskbar 0 /pin-additional-shortcuts 0 /allusers";
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("Opera GX",
-                "107.0.5045.70",
+                "107.0.5045.75",
                 "^Opera GX Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 "^Opera GX Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera_gx/107.0.5045.70/win/Opera_GX_107.0.5045.70_Setup.exe",
+                    "https://get.geo.opera.com/pub/opera_gx/107.0.5045.75/win/Opera_GX_107.0.5045.75_Setup.exe",
                     HashAlgorithm.SHA256,
-                    "e9a689819beab2ff0cc2969b57d38b474618a63113cf49d5b34906dc97c6080c",
+                    "d53d149362e1cc893db6eb84367a307f03d939c334255abeec2521ed0b1839a9",
                     signature,
                     silentOptions),
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera_gx/107.0.5045.70/win/Opera_GX_107.0.5045.70_Setup_x64.exe",
+                    "https://get.geo.opera.com/pub/opera_gx/107.0.5045.75/win/Opera_GX_107.0.5045.75_Setup_x64.exe",
                     HashAlgorithm.SHA256,
-                    "6951f3b0e51125e339a5d16bd41c4bfbe0da56ff64dbba7f0adf07c07d8fb7c9",
+                    "56eab92787a1df45ce7974f7ca772ede13312ee5a7c6e7d0361402484c5760c5",
                     signature,
                     silentOptions)
                     );
