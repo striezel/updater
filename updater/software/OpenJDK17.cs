1b3b7d566c933390dedb3139610d4b873822b87a
Dirk Stolle <striezel-dev@web.de>
2024-04-22 19:29:39 +0200

    update known information for Eclipse Temurin JDK 17 to version 17.0.11+9

--- updater/software/OpenJDK17.cs
+++ updater/software/OpenJDK17.cs
@@ -67,19 +67,19 @@ namespace updater.software
         {
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("Eclipse Temurin JDK 17 with Hotspot",
-                "17.0.10.7",
+                "17.0.11.9",
                 "^Eclipse Temurin JDK [a-z]+ Hotspot 17\\.[0-9]+\\.[0-9]+\\+[0-9]+(\\.[0-9]+)? \\(x86\\)$",
                 "^Eclipse Temurin JDK [a-z]+ Hotspot 17\\.[0-9]+\\.[0-9]+\\+[0-9]+(\\.[0-9]+)? \\(x64\\)$",
                 new InstallInfoMsiNoLocation(
-                    "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.10_7.msi",
+                    "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11%2B9/OpenJDK17U-jdk_x86-32_windows_hotspot_17.0.11_9.msi",
                     HashAlgorithm.SHA256,
-                    "f30b0dcd1b0f95e6ccca712fe1cbe0f8ba577ae5ccef28b08a1c77c7209fdc7c",
+                    "82345aa7e4af7e0ff245bf7aec60e5ba2fe4c070285950869febda9bb310a5b5",
                     signature,
                     "INSTALLLEVEL=3 /qn /norestart"),
                 new InstallInfoMsiNoLocation(
-                    "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.10%2B7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.10_7.msi",
+                    "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11%2B9/OpenJDK17U-jdk_x64_windows_hotspot_17.0.11_9.msi",
                     HashAlgorithm.SHA256,
-                    "d45b610b1800e35fd667d144277272e3323c7fafa21c0312649fc60a86913a3d",
+                    "99ad599eea572207026f2b85159ef4ec06c620cee88662ea108f0013697b0365",
                     signature,
                     "INSTALLLEVEL=3 /qn /norestart")
                     );
