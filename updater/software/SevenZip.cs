b28805818dea35bf532ef93df35a65d4fc1727fc
Dirk Stolle <striezel-dev@web.de>
2024-05-16 07:11:43 +0200

    update known information for 7-Zip to version 24.05

--- updater/software/SevenZip.cs
+++ updater/software/SevenZip.cs
@@ -1,6 +1,6 @@
 ﻿/*
     This file is part of the updater command line interface.
-    Copyright (C) 2017, 2018, 2019, 2021, 2022, 2023  Dirk Stolle
+    Copyright (C) 2017, 2018, 2019, 2021, 2022, 2023, 2024  Dirk Stolle
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -52,19 +52,19 @@ namespace updater.software
         public override AvailableSoftware knownInfo()
         {
             return new AvailableSoftware("7-Zip",
-                "23.01",
+                "24.05",
                 "^7\\-Zip [0-9]+\\.[0-9]{2}$",
                 "^7\\-Zip [0-9]+\\.[0-9]{2} \\(x64\\)$",
                 new InstallInfoExe(
-                    "https://www.7-zip.org/a/7z2301.exe",
+                    "https://www.7-zip.org/a/7z2405.exe",
                     HashAlgorithm.SHA256,
-                    "9b6682255bed2e415bfa2ef75e7e0888158d1aaf79370defaa2e2a5f2b003a59",
+                    "3194526f123508f9fa56c66e767738e2648fd491c44531af28323a126a679c5d",
                     Signature.None,
                     "/S"),
                 new InstallInfoExe(
-                    "https://www.7-zip.org/a/7z2301-x64.exe",
+                    "https://www.7-zip.org/a/7z2405-x64.exe",
                     HashAlgorithm.SHA256,
-                    "26cb6e9f56333682122fafe79dbcdfd51e9f47cc7217dccd29ac6fc33b5598cd",
+                    "12ef1c8127ec3465520e4cfd23605b708d81a5a2cf37ba124f018e5c094de0d9",
                     Signature.None,
                     "/S")
                 );
