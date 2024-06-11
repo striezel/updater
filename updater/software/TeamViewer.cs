696d8a8b82c9b62cc14d8808137509337782b7b7
Dirk Stolle <striezel-dev@web.de>
2024-06-11 23:32:59 +0200

    update known version for TeamViewer to 15.54.6

--- updater/software/TeamViewer.cs
+++ updater/software/TeamViewer.cs
@@ -66,7 +66,7 @@ namespace updater.software
         {
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("TeamViewer",
-                "15.54.5",
+                "15.54.6",
                 "^TeamViewer$",
                 "^TeamViewer$",
                 new InstallInfoExe(
