4a23b62a7c00748717bc54e11d3be93bad22c9f9
Dirk Stolle <striezel-dev@web.de>
2024-11-06 12:20:10 +0100

    fix signature verification test
    The signature of the PuTTY installer expired today, so we switch
    to the LibreOffice Help Pack installer instead which has a
    signature that has not expired yet.

--- updater-test/utility/Verificator_Tests.cs
+++ updater-test/utility/Verificator_Tests.cs
@@ -36,9 +36,9 @@ namespace updater_test.utility
 
 
         /// <summary>
-        /// subject in signature for PuTTY installer
+        /// subject in signature for LibreOffice Help Pack installer
         /// </summary>
-        private const string puttyPublisherX509 = "CN=Simon Tatham, O=Simon Tatham, S=Cambridgeshire, C=GB";
+        private const string libreOfficePublisherX509 = "E=info@documentfoundation.org, CN=The Document Foundation, O=The Document Foundation, OU=LibreOffice Build Team, L=Berlin, S=Berlin, C=DE";
 
 
         /// <summary>
@@ -79,7 +79,7 @@ namespace updater_test.utility
         [ClassInitialize()]
         public static void DownloadExampleFile(TestContext testContext)
         {
-            downloadFileLocation = Download("https://the.earth.li/~sgtatham/putty/0.81/w32/putty-0.81-installer.msi");
+            downloadFileLocation = Download("https://download.documentfoundation.org/libreoffice/stable/24.8.2/win/x86_64/LibreOffice_24.8.2_Win_x86-64_helppack_de.msi");
         }
 
 
@@ -126,7 +126,7 @@ namespace updater_test.utility
                     stream.Close();
                 }
                 // check signature
-                verified = updater.utility.Verificator.verifySignature(copyLocation, puttyPublisherX509);
+                verified = updater.utility.Verificator.verifySignature(copyLocation, libreOfficePublisherX509);
             }
             finally
             {
@@ -146,8 +146,8 @@ namespace updater_test.utility
         {
             Assert.IsNotNull(downloadFileLocation, "The test file was not downloaded!");
 
-            bool s = updater.utility.Verificator.verifySignature(downloadFileLocation, puttyPublisherX509);
-            // If this assertion fails and it is the 6th November 2024 or later,
+            bool s = updater.utility.Verificator.verifySignature(downloadFileLocation, libreOfficePublisherX509);
+            // If this assertion fails and it is the 13th September 2026 or later,
             // then this is because the certificate has expired.
             Assert.IsTrue(s);
         }
