f103c8a7f272252eb9c957713ffb2123d04ffc05
Dirk Stolle <striezel-dev@web.de>
2024-11-27 23:21:49 +0100

    update known information for Opera to version 115.0.5322.68

--- updater/software/Opera.cs
+++ updater/software/Opera.cs
@@ -66,19 +66,19 @@ namespace updater.software
             const string silentOptions = "/silent /norestart /launchopera 0 /setdefaultbrowser 0 /enable-stats 0 /enable-installer-stats 0 /pintotaskbar 0 /pin-additional-shortcuts 0 /allusers";
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("Opera",
-                "114.0.5282.222",
+                "115.0.5322.68",
                 "^Opera Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 "^Opera Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera/desktop/114.0.5282.222/win/Opera_114.0.5282.222_Setup.exe",
+                    "https://get.geo.opera.com/pub/opera/desktop/115.0.5322.68/win/Opera_115.0.5322.68_Setup.exe",
                     HashAlgorithm.SHA256,
-                    "57d1e227088a62b6bb0122645b16f1003690b874b988c322aadaa54ea6d4c2b8",
+                    "7d6344bd3792524711d807e063b181357338351c88f3865932edd808b6a44910",
                     signature,
                     silentOptions),
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera/desktop/114.0.5282.222/win/Opera_114.0.5282.222_Setup_x64.exe",
+                    "https://get.geo.opera.com/pub/opera/desktop/115.0.5322.68/win/Opera_115.0.5322.68_Setup_x64.exe",
                     HashAlgorithm.SHA256,
-                    "618a129e3a8edbb1443d7be23d1a8304d2be8e8629f60907a53a7c3131dbbf1d",
+                    "421745f4c3700c31e88ad939f879bd96da5a18596d7c95300362fbc5c8f9379a",
                     signature,
                     silentOptions)
                     );
