4ea72e2b08705f10823747477c38fa509cdf9d24
Dirk Stolle <striezel-dev@web.de>
2024-11-28 23:18:10 +0100

    update known information for Opera GX to version 114.0.5282.243

--- updater/software/OperaGX.cs
+++ updater/software/OperaGX.cs
@@ -66,19 +66,19 @@ namespace updater.software
             const string silentOptions = "/silent /norestart /launchopera 0 /setdefaultbrowser 0 /enable-stats 0 /enable-installer-stats 0 /pintotaskbar 0 /pin-additional-shortcuts 0 /allusers";
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("Opera GX",
-                "114.0.5282.218",
+                "114.0.5282.243",
                 "^Opera GX Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 "^Opera GX Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera_gx/114.0.5282.218/win/Opera_GX_114.0.5282.218_Setup.exe",
+                    "https://get.geo.opera.com/pub/opera_gx/114.0.5282.243/win/Opera_GX_114.0.5282.243_Setup.exe",
                     HashAlgorithm.SHA256,
-                    "70e659cf024d181f0ac1779abd2a8996ea50f225b8f32f6b2dc73c7dff520e40",
+                    "a18e866378572d86f874f897c423f59185def10dfaf797d07e4caf14bb79d22e",
                     signature,
                     silentOptions),
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera_gx/114.0.5282.218/win/Opera_GX_114.0.5282.218_Setup_x64.exe",
+                    "https://get.geo.opera.com/pub/opera_gx/114.0.5282.243/win/Opera_GX_114.0.5282.243_Setup_x64.exe",
                     HashAlgorithm.SHA256,
-                    "34de00905f5c031bd0b0a529a97b8a96f26c1460614fcfc385ef1826498191ad",
+                    "7674be1020ae3a0d79490efc0aef09efe17bc5ee00ae134f5b761b6801e1955c",
                     signature,
                     silentOptions)
                     );
