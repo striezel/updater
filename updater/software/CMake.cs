e0f12af69e33377b96b9281f018ea0b91b9580e8
Dirk Stolle <striezel-dev@web.de>
2025-03-28 06:42:35 +0100

    adjust wrong capacity in pre-allocated list

--- updater/software/CMake.cs
+++ updater/software/CMake.cs
@@ -193,7 +193,7 @@ namespace updater.software
         /// <returns>Returns a list of process names that block the upgrade.</returns>
         public override List<string> blockerProcesses(DetectedSoftware detected)
         {
-            return new List<string>(3)
+            return new List<string>(4)
             {
                 "cmake",
                 "cmake-gui",
