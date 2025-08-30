5724ebaf863b658ccf5a27a68168b9950855b804
Dirk Stolle <striezel-dev@web.de>
2025-08-30 09:46:08 +0200

    update known information for LibreWolf to version 142.0.1-1

--- updater/software/LibreWolf.cs
+++ updater/software/LibreWolf.cs
@@ -56,19 +56,19 @@ namespace updater.software
         public override AvailableSoftware knownInfo()
         {
             return new AvailableSoftware("LibreWolf",
-                "142.0-1",
+                "142.0.1-1",
                 "^LibreWolf$",
                 "^LibreWolf$",
                 new InstallInfoExe(
-                    "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/142.0-1/librewolf-142.0-1-windows-i686-setup.exe",
+                    "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/142.0.1-1/librewolf-142.0.1-1-windows-i686-setup.exe",
                     HashAlgorithm.SHA256,
-                    "a1bfd6804eed7a33ca1ac21d52c28fce9d9ebc7e46ef0b457858eeb842ec6edc",
+                    "b101b11357a1c5d1ac925ce517f8e693339299826c8f08a477647d057c84ca7c",
                     Signature.None,
                     "/S"),
                 new InstallInfoExe(
-                    "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/142.0-1/librewolf-142.0-1-windows-x86_64-setup.exe",
+                    "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/142.0.1-1/librewolf-142.0.1-1-windows-x86_64-setup.exe",
                     HashAlgorithm.SHA256,
-                    "1e42a52d415c5ee82ce485e357b4baad700daacc2a7e09b04e7096d7cf8ce658",
+                    "df1a0e2778d53751cd5f33dc2389bfd1ce4a9c1535681a7dbfcf3335e4b00157",
                     Signature.None,
                     "/S")
                 );
