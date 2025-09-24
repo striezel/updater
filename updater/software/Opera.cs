6a2dfd21fa10c3db1c086eb8467d28136324e833
Dirk Stolle <striezel-dev@web.de>
2025-09-24 22:41:55 +0200

    update known information for Opera to version 122.0.5643.71

--- updater/software/Opera.cs
+++ updater/software/Opera.cs
@@ -66,19 +66,19 @@ namespace updater.software
             const string silentOptions = "/silent /norestart /launchopera 0 /setdefaultbrowser 0 /enable-stats 0 /enable-installer-stats 0 /pintotaskbar 0 /pin-additional-shortcuts 0 /allusers";
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("Opera",
-                "122.0.5643.51",
+                "122.0.5643.71",
                 "^Opera Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 "^Opera Stable [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+$",
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera/desktop/122.0.5643.51/win/Opera_122.0.5643.51_Setup.exe",
+                    "https://get.geo.opera.com/pub/opera/desktop/122.0.5643.71/win/Opera_122.0.5643.71_Setup.exe",
                     HashAlgorithm.SHA256,
-                    "0f8bdf420df14fe4d467e523fb050f1bc617df14defe5568b611ecb7fe9bd552",
+                    "3e9714c7add62947989dcce6d18573904784cdd0f40379f4b595fa61d030f3e0",
                     signature,
                     silentOptions),
                 new InstallInfoExe(
-                    "https://get.geo.opera.com/pub/opera/desktop/122.0.5643.51/win/Opera_122.0.5643.51_Setup_x64.exe",
+                    "https://get.geo.opera.com/pub/opera/desktop/122.0.5643.71/win/Opera_122.0.5643.71_Setup_x64.exe",
                     HashAlgorithm.SHA256,
-                    "9d325144d7935b2e74275c576e26c0c39b2c43c6dfc5f690047ab2828f98a679",
+                    "ad157c01c822b54b33cbbf10030a50db94315d85a45ddc6173e6f14aabc7c90a",
                     signature,
                     silentOptions)
                     );
