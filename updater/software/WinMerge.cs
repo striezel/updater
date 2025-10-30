82135d980e39f2723ad10b9389dcf3e7fb3c6fd2
Dirk Stolle <striezel-dev@web.de>
2025-10-30 23:34:27 +0100

    update known information for WinMerge to version 2.16.52

--- updater/software/WinMerge.cs
+++ updater/software/WinMerge.cs
@@ -66,19 +66,19 @@ namespace updater.software
         {
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("WinMerge",
-                "2.16.50.2",
+                "2.16.52.0",
                 "^WinMerge [0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+?$",
                 "^WinMerge ([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+ )?x64$",
                 new InstallInfoExe(
-                    "https://netcologne.dl.sourceforge.net/project/winmerge/stable/2.16.50.2/WinMerge-2.16.50.2-Setup.exe",
+                    "https://netcologne.dl.sourceforge.net/project/winmerge/stable/2.16.52/WinMerge-2.16.52-Setup.exe",
                     HashAlgorithm.SHA256,
-                    "28feeb222a177386df65053740ab067ab99420df1651a433cd7bcd4eab6f4898",
+                    "d6ff1b9f6046d4fd8d4426fdfd5415d8e7ee60dbbc62c0b3487f69011cb56c12",
                     signature,
                     "/VERYSILENT /NORESTART"),
                 new InstallInfoExe(
-                    "https://netcologne.dl.sourceforge.net/project/winmerge/stable/2.16.50.2/WinMerge-2.16.50.2-x64-Setup.exe",
+                    "https://netcologne.dl.sourceforge.net/project/winmerge/stable/2.16.52/WinMerge-2.16.52-x64-Setup.exe",
                     HashAlgorithm.SHA256,
-                    "49f7185f6253df66535356baf0e43a15f184efdafe9db7a25f6a579c63e86870",
+                    "8643aa9937f02def3da161ab11e04d1f45a7961f60a47d1358e36f0dbdb6289e",
                     signature,
                     "/VERYSILENT /NORESTART")
                 );
