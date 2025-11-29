4b4427bdaeee6778fea1268f5963f60e0c9396de
Dirk Stolle <striezel-dev@web.de>
2025-11-29 23:48:25 +0100

    update known information for Graphviz to version 14.0.5

--- updater/software/Graphviz.cs
+++ updater/software/Graphviz.cs
@@ -56,19 +56,19 @@ namespace updater.software
         public override AvailableSoftware knownInfo()
         {
             return new AvailableSoftware("Graphviz",
-                "14.0.4",
+                "14.0.5",
                 "^Graphviz$",
                 "^Graphviz$",
                 new InstallInfoExe(
-                    "https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/14.0.4/windows_10_cmake_Release_graphviz-install-14.0.4-win32.exe",
+                    "https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/14.0.5/windows_10_cmake_Release_graphviz-install-14.0.5-win32.exe",
                     HashAlgorithm.SHA256,
-                    "0aa3b39d3635e812b1983a06486cc50286d41a5bc06bc0f10c41292da38da262",
+                    "a1414d7ac43370b856dcbc8f737da28e019c5da1efc85d368242fa22f50f3495",
                     Signature.None,
                     "/S"),
                 new InstallInfoExe(
-                    "https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/14.0.4/windows_10_cmake_Release_graphviz-install-14.0.4-win64.exe",
+                    "https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/14.0.5/windows_10_cmake_Release_graphviz-install-14.0.5-win64.exe",
                     HashAlgorithm.SHA256,
-                    "478c9d38f8cd05dae453d1e5053ac197329fd79fb5e13e52d25f7812cb8bda3d",
+                    "f4a369adacd6261d5faaa3e8450f33b0625b88c8c14ec94af3bfcaba99f0fdfe",
                     Signature.None,
                     "/S")
                 );
