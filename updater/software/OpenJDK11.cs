5109cdd254e77cb8bc4ad7b4c1f0e206e9dd1e51
Dirk Stolle <striezel-dev@web.de>
2026-03-04 22:12:59 +0100

    update known information for Eclipse Temurin JDK 11 to version 11.0.30+7

--- updater/software/OpenJDK11.cs
+++ updater/software/OpenJDK11.cs
@@ -67,14 +67,14 @@ namespace updater.software
         {
             var signature = new Signature(publisherX509, certificateExpiration);
             return new AvailableSoftware("Eclipse Temurin JDK 11 with Hotspot",
-                "11.0.29.7",
+                "11.0.30.7",
                 null,
                 "^(Eclipse Temurin JDK [a-z]+ Hotspot 11\\.[0-9]+\\.[0-9]+\\+[0-9]+(\\.[0-9]+)? \\(x64\\)|AdoptOpenJDK JDK [a-z]+ Hotspot 11\\.[0-9]+\\.[0-9]+\\+[0-9]+(\\.[0-9]+)? \\(x64\\))$",
                 null,
                 new InstallInfoMsiNoLocation(
-                    "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.29%2B7/OpenJDK11U-jdk_x64_windows_hotspot_11.0.29_7.msi",
+                    "https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.30%2B7/OpenJDK11U-jdk_x64_windows_hotspot_11.0.30_7.msi",
                     HashAlgorithm.SHA256,
-                    "219e88a862ccdde521c42d8ebc001cc711279cdef79157b568ec2ae16b3abde0",
+                    "5a7e985d6e89c864cf176c9ebf919ab1e97b40877b8e5290e4c51f532e37d1c2",
                     signature,
                     "INSTALLLEVEL=3 /qn /norestart")
                     );
