f817312309920db73b01f7d89b1ea6243c205e55
Dirk Stolle <striezel-dev@web.de>
2026-03-15 22:18:41 +0100

    update known information for GIMP to version 3.2.0

--- updater/software/GIMP.cs
+++ updater/software/GIMP.cs
@@ -69,14 +69,14 @@ namespace updater.software
         public override AvailableSoftware knownInfo()
         {
             var installer = new InstallInfoExe(
-                "https://download.gimp.org/gimp/v3.0/windows/gimp-3.0.8-setup-2.exe",
+                "https://download.gimp.org/gimp/v3.2/windows/gimp-3.2.0-setup.exe",
                 HashAlgorithm.SHA256,
-                "3e4ff5845126a026e22302ab0aeecb0a79caff495f7675e254e1da4878cd3d72",
+                "6332ab8fdf9d1f229abd7aab18874d476ae31405e2fe75bf786b8fdc90b891a8",
                 new Signature(publisherX509, certificateExpiration),
                 "/VERYSILENT /NORESTART /ALLUSERS");
 
             return new AvailableSoftware("The GIMP",
-                "3.0.8.2",
+                "3.2.0",
                 "^GIMP [0-9]+\\.[0-9]+\\.[0-9]+(\\-[0-9]+)?$",
                 "^GIMP [0-9]+\\.[0-9]+\\.[0-9]+(\\-[0-9]+)?$",
                 // The GIMP uses the same installer for 32 and 64-bit.
